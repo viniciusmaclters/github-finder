57d670dc920765b59e0f62031951fa377207eee0
Vinicius Maclters <vinicius.maclters@hotmail.com>
2023-03-14 09:42:02 -0300

    useState

---
+++ src/components/Search.tsx
@@ -0,0 +1,28 @@
+import { BsSearch } from 'react-icons/bs'
+import { useState } from 'react'
+
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>
+}
+
+export function Search({loadUser}: SearchProps) {
+  const [userName, setUserName] = useState("")
+
+  return (
+    <div>
+      <h2>Busque por um usuário:</h2>
+      <p>Conheça seus melhores repositórios</p>
+      <div>
+        <input 
+          type="text" 
+          placeholder="Digite o nome do usuário" 
+          onChange={(e) => setUserName(e.target.value)} 
+        />
+
+        <button onClick={() => loadUser(userName)}>
+          <BsSearch />
+        </button>
+      </div>
+    </div>
+  )
+}
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,21 @@
+import { UserProps } from "../types/user"
+import { Search } from "../components/Search"
+
+import { useState } from "react"
+
 export function Home() {
+  const [user, setUser] = useState<UserProps | null>(null)
+
+  const loadUser = async(userName: string) => {
+    const response = await fetch(`https://api.github.com/users/${userName}`)
+    const data = await response.json()
+    
+    console.log(data)
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      <Search loadUser={loadUser} />  
+    </div>
   )
 }
